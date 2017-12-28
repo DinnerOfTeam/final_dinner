988b351b6ca528476005986d48334c0a55d6e222
user <user@user-PC>
2017-12-28 19:36:54 +0900

    수정

---
+++ final_dinner/src/main/java/com/finalTotal/dinner/food/model/FoodMenuServiceImpl.java
@@ -0,0 +1,30 @@
+package com.finalTotal.dinner.food.model;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class FoodMenuServiceImpl implements FoodMenuService{
+
+	
+	@Autowired
+	private FoodMenuDAO foodMenuDao;
+	
+	@Override
+	public int insertFoodMenu(FoodItemVO vo) {
+		return foodMenuDao.insertFoodMenu(vo);
+	}
+
+	@Override
+	public int deleteMenu(int foodItemNo) {
+		return foodMenuDao.deleteMenu(foodItemNo);
+	}
+
+	@Override
+	public List<FoodItemVO> selectAll(int foodItemNo) {
+		return foodMenuDao.selectAll(foodItemNo);
+	}
+
+}
