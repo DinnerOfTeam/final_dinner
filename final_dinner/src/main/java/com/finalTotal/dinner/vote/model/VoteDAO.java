64b48eb26d47079cf26c2a28f98e6af93616f798
eun <kyl0901@naver.com>
2017-12-29 18:21:43 +0900

    투표 항목추가 완료

--- final_dinner/src/main/java/com/finalTotal/dinner/vote/model/VoteDAO.java
+++ final_dinner/src/main/java/com/finalTotal/dinner/vote/model/VoteDAO.java
@@ -2,4 +2,5 @@ package com.finalTotal.dinner.vote.model;
 
 public interface VoteDAO {
 	public int insertVote(VoteVO vo);
+	public int insertItem(Vote_ItemVO vo);
 }
--- final_dinner/src/main/java/com/finalTotal/dinner/vote/model/VoteService.java
+++ final_dinner/src/main/java/com/finalTotal/dinner/vote/model/VoteService.java
@@ -1,5 +1,7 @@
 package com.finalTotal.dinner.vote.model;
 
+import java.util.List;
+
 public interface VoteService {
-	public int insertVote(VoteVO vo);
+	public int insertVote(VoteVO vo, List<Vote_ItemVO> list);
 }
